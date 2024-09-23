16ba0854333e5a523526d2e45c9b57dbe1395d9a
andTheowind <59265638+andTheowind@users.noreply.github.com>
2024-09-23 16:43:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vite-deploy-demo/'
-
+  base: process.env.NODE_ENV === 'production' ? '/projectVue/' : '/',
 })
 
 
